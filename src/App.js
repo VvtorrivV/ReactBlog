1ed68858405d868430156ce22462c73e844ea00b
VvtorrivV <wikcia.sagan@gmail.com>
2022-03-08 10:26:17 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = 'Welcome to Blog';
+  const likes = 50;
+  const person = {name: 'vimes', age: 30};
+  const link='https://www.google.com';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        {/*Error - nie można przekazać całego obiektu
+        <p>{person}</p>
+        */}
+        <p>{person.age}</p>
+        <p>{'You can put any variable in brackets'}</p>
+        <p>{[1,2,3,4,5]}</p>
+        <a href={link}>Google</a>
+      </div>
     </div>
   );
 }
